cae1ffc526e6672d45a2cdfe54cf67af152c92ab
Maryam Tofeeq <maryamtofeeq7@gmail.com>
2026-09-14 22:32:30 +0500

    data type

---
+++ class.py
@@ -0,0 +1,21 @@
+print("the sun rise in the morning")
+print(("maryam")*10)
+name = "davil"
+print(type(name))
+age= "fals"
+print(type(age))
+print(("maryam/n")*9)
+print(("maryam/n")*10)
+name="maryam"
+age = 17
+mask = 99.5
+
+print(type(mask))
+x = 25
+y = 99.5
+
+
+result = x + y
+print(result)
+x = "19"
+print((type)xs)
