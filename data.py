1edb88ef06c416a328e4b2b156b9fc657cbef528
Maryam Tofeeq <maryamtofeeq7@gmail.com>
2026-09-15 21:07:49 +0500

    concatenation|

---
+++ data.py
@@ -0,0 +1,12 @@
+print("name")
+print("sting")
+print(("maryam/n")*10)
+name = "maryam"
+age = 10
+
+print(type(age))
+passed = True
+
+print(type(passed))
+name = input("enter your name :")
+print ("hello" = name)
