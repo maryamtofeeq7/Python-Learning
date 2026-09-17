e4b1457b460f0a648b0a801fcde7326c92c3acb0
Maryam Tofeeq <maryamtofeeq7@gmail.com>
2026-09-17 20:48:52 +0500

    concatenation

--- data.py
+++ data.py
@@ -4,9 +4,13 @@ print(("maryam/n")*10)
 name = "maryam"
 age = 10
 
-print(type(age))
+print(type(age , name))
 passed = True
 
 print(type(passed))
 name = input("enter your name :")
 print ("hello" = name)
+name = "maryam"
+age = 27
+
+print(type(age))
