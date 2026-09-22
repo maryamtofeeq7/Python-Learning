0551b9d5f782cb9aa54fb40a6c15955fa800c10f
Maryam Tofeeq <maryamtofeeq7@gmail.com>
2026-09-22 21:13:30 +0500

    learning concatenation index slicing  method

--- shahbaz.py
+++ shahbaz.py
@@ -1,3 +1,1 @@
 
-print(new_agage = input (enter your age)
-new_age+int(age)+1e)
---
+++ tofeeq.py
@@ -0,0 +1,21 @@
+print("shahbaz")    #sting
+name = ("shahbaz") # DATA TYPE
+print(type(name))
+age = 17
+print (type (age))
+x = 20 # variable
+print (x)
+name = "ayaz"  # variable
+print(name)
+x = "hellow"
+y = "world"
+
+print(x + y)
+name = "ahmad"
+print(len(name))
+word = "pakistan"   #index
+print(word[3])
+word="phython" #slicing
+print (word[0:3])
+name = "pakistan"
+print(name.upper())
